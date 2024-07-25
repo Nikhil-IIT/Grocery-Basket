c308453aa943b4db4ad06041e800f50f137a2558
NIKHIL <100149153+Nikhil-IIT@users.noreply.github.com>
2024-07-26 00:38:41 +0530

    Update app.py

--- python/app.py
+++ python/app.py
@@ -643,5 +643,9 @@ def delete_category(c_id):
     db.session.commit()
     return jsonify({'message': 'Deleted successful'})      
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
